c78007d5bc67acabd0c3330e0f81cfa755252b3f
carlos <huaixian.huang@gmail.com>
2018-11-30 16:32:10 +0800

    feat: get click area text

--- contentScript.js
+++ contentScript.js
@@ -3,7 +3,7 @@ var axios = require('axios');
 var lodash = require('lodash');
 var gitlab = require('./gitlab');
 
-console.log('extension loaded');
+console.log('Gitlab Merge Request image preview extension loaded');
 
 var gitlab_axios_instance = axios.create({
   baseURL: config.api_url,
@@ -11,7 +11,50 @@ var gitlab_axios_instance = axios.create({
   headers: { "PRIVATE-TOKEN": config.token}
 });
 
+function getText(node, lineSeparator){
+    var text = "";
+    while(node != null){
+        if(node.nodeType == Node.TEXT_NODE){
+            text += node.nodeValue.trim();
+        }else if(node.nodeType == Node.ELEMENT_NODE){
+            if($(node).is(':visible')){
+                var childText = ""; 
+                if(node.firstChild != null){
+                    if(node.parentNode.nodeName == "SELECT" && node.nodeName == "OPTION"){
+                        // Get selected option text only
+                        if(node.parentNode[node.parentNode.selectedIndex] == node){
+                            childText = getText(node.firstChild, lineSeparator);
+                        }
+                    }else{
+                        childText = getText(node.firstChild, lineSeparator);
+                    }
+                }
+
+                if(childText != ""){
+                    text += childText;
+                }
+
+                if(node.nodeName == "BR" || window.getComputedStyle(node, null).display.indexOf("inline") === -1){
+                    text += lineSeparator;
+                }
+            }
+        }
+
+        node = node.nextSibling;
+    }
+
+    return text.trim();
+}
+
 window.addEventListener ("load", myMain, false);
+
+document.addEventListener("click", function(event){
+  console.log('click event is:',event);
+  if (event.metaKey)  {
+    event.preventDefault();
+    console.log('first child is:',getText(event.target.firstChild,"\r\n"));
+  }
+}, false);
 
 function myMain () {
 
